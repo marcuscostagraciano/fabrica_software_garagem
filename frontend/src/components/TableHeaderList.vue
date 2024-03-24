7c77a020ab92e74f33e9c97eaf9b0cc2372feb29
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-24 13:46:52 -0300

    componentização

---
+++ frontend/src/components/TableHeaderList.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps(["th_text"])
+</script>
+
+<template>
+    <thead>
+        <tr>
+            <th>
+                Código
+            </th>
+            <th v-if="props.th_text" v-for="text in props.th_text">
+                {{ text }}
+            </th>
+            <th v-else>
+                Descrição
+            </th>
+            <th>
+                Excluir
+            </th>
+        </tr>
+    </thead>
+</template>
+
+<style scoped></style>
