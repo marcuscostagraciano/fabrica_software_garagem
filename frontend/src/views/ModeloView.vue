77025b2c0aadf7aa19216cd83f15e5c78a35a087
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-24 21:00:57 -0300

    Criação telas: Modelo e Veículo

--- frontend/src/views/ModeloView.vue
+++ frontend/src/views/ModeloView.vue
@@ -1,9 +1,139 @@
 <script setup>
+import { computed, ref, reactive, onMounted } from "vue";
+
+import TableHeaderList from "../components/TableHeaderList.vue";
+
+import CategoriasApi from "@/api/categorias"
+import MarcasApi from "@/api/marcas"
+import ModelosApi from "@/api/modelos";
+
+const categoriasApi = new CategoriasApi();
+const marcasApi = new MarcasApi();
+const modelosApi = new ModelosApi();
+
+const categorias = ref([])
+const lista_categorias = ref([])
+
+const marcas = ref([])
+const lista_marcas = ref([])
+
+const defaultModelo = { id: null, nome: "", categoria: null, marca: null };
+const modelos = ref([]);
+const modelo = reactive({ ...defaultModelo });
+
+const isEditando = ref(false)
+
+onMounted(async () => {
+    modelos.value = await modelosApi.getAll();
+
+    categorias.value = await categoriasApi.getAll();
+    for (let _ in categorias.value) {
+        lista_categorias.value.push({
+            id: Number(_) + 1,
+            descricao: categorias.value[_]["descricao"]
+        })
+    }
+
+    marcas.value = await marcasApi.getAll();
+    for (let _ in marcas.value) {
+        lista_marcas.value.push({
+            id: Number(_) + 1,
+            nome: marcas.value[_]["nome"]
+        })
+    }
+});
+
+function limpar() {
+    isEditando.value = false
+    Object.assign(modelo, { ...defaultModelo });
+}
+
+async function salvar() {
+    if (modelo.id) {
+        await modelosApi.update(modelo);
+    } else {
+        await modelosApi.add(modelo);
+    }
+    modelos.value = await modelosApi.getAll();
+    limpar();
+}
+
+function editar(modelo_para_editar) {
+    isEditando.value = true
+    Object.assign(modelo, modelo_para_editar);
+}
+
+async function excluir(id) {
+    await modelosApi.delete(id);
+    modelos.value = await modelosApi.getAll();
+    limpar();
+}
+
+const theader_text = [
+    "Nome", "Categoria",
+    "Marca"
+]
 
 </script>
 
 <template>
+    <h1 class="my-3">Modelos</h1>
+    <v-form class="mx-6">
+        <v-text-field v-model="modelo.nome" name="nome" label="Nome"></v-text-field>
+
+        <v-select :items="lista_categorias" item-title="descricao" item-value="id" v-model="modelo.categoria"
+            label="Categoria"></v-select>
+        <v-select :items="lista_marcas" item-title="nome" item-value="id" v-model="modelo.marca"
+            label="Marca"></v-select>
+
+        <v-container class="d-flex justify-center">
+            <v-btn @click="salvar" class="bg-surface-variant">
+                <p v-if="!isEditando">Adicionar</p>
+                <p v-else>Atualizar</p>
+            </v-btn>
+            <v-btn @click="limpar" class="bg-surface-variant">Limpar</v-btn>
+        </v-container>
+    </v-form>
+    <hr />
+
+    <v-table density="comfortable">
+        <TableHeaderList :th_text="theader_text" />
+        <tbody>
+            <tr v-for="modelo in modelos" :key="modelo.name" @click="editar(modelo)">
+                <td>{{ modelo.id }}</td>
+                <td>{{ modelo.nome }}</td>
+
+                <td>{{ lista_categorias[modelo.categoria - 1]["descricao"] }}</td>
+                <td>{{ lista_marcas[modelo.marca - 1]["nome"] }}</td>
+
+                <!-- Que sirvam de cicatrizes -->
+                <!-- <td>{{ modelo.categoria }}</td> -->
+                <!-- <td>{{ categorias.filter((categoria) => categoria['id'] == modelo.categoria)[0]["descricao"] }}</td> -->
+                <!-- <td>{{ getDescCategoria(modelo.categoria)["descricao"] }}</td> -->
+                <!-- <td>{{ getDescMarca(modelo.marca)["nome"] }}</td> -->
+                <!-- <td>{{ modelo.marca }}</td> -->
+                <td><button @click="excluir(modelo.id)" class="excluir">X</button></td>
+            </tr>
+        </tbody>
+    </v-table>
 </template>
 
+<style>
+table {
+    text-align: center !important;
+}
 
-<style scoped></style>
+thead tr th {
+    text-align: center !important;
+    color: #fff;
+    background-color: #a6a29a;
+    text-transform: uppercase;
+}
+
+.excluir {
+    border-radius: 20px;
+    height: 50%;
+    width: 50%;
+    background-color: red;
+}
+</style>
--- frontend/src/views/VeiculoView.vue
+++ frontend/src/views/VeiculoView.vue
@@ -1,9 +1,110 @@
 <script setup>
+import { ref, reactive, onMounted } from "vue";
+
+import TableHeaderList from "../components/TableHeaderList.vue";
+
+import VeiculosApi from "@/api/veiculos";
+const veiculosApi = new VeiculosApi();
+
+const defaultVeiculo = {
+    id: null, cor: null, modelo: null,
+    ano: null, descricao: "", preco: null
+};
+const veiculos = ref([]);
+const veiculo = reactive({ ...defaultVeiculo });
+const isEditando = ref(false)
+
+onMounted(async () => {
+    veiculos.value = await veiculosApi.getAll();
+    // console.log(veiculos)
+    // console.log(veiculos.value)
+    // console.log(Object.getOwnPropertyNames(defaultVeiculo).slice(1,));
+});
+
+function limpar() {
+    isEditando.value = false
+    Object.assign(veiculo, { ...defaultVeiculo });
+}
+
+async function salvar() {
+    if (veiculo.id) {
+        await veiculosApi.update(veiculo);
+    } else {
+        await veiculosApi.add(veiculo);
+    }
+    veiculos.value = await veiculosApi.getAll();
+    limpar();
+}
+
+function editar(veiculo_para_editar) {
+    isEditando.value = true
+    Object.assign(veiculo, veiculo_para_editar);
+}
+
+async function excluir(id) {
+    await veiculosApi.delete(id);
+    veiculos.value = await veiculosApi.getAll();
+    limpar();
+}
+
+const theader_text = [
+    "Cor", "Modelo", "Ano",
+    "Descrição", "Preço"
+]
 
 </script>
 
 <template>
+    <h1 class="my-3">Veículos</h1>
+    <v-form class="mx-6">
+        <v-text-field v-model="veiculo.cor" name="cor" label="Cor"></v-text-field>
+        <v-text-field v-model="veiculo.modelo" name="modelo" label="Modelo"></v-text-field>
+        <v-text-field v-model="veiculo.ano" name="ano" label="Ano" type="number"></v-text-field>
+        <v-text-field v-model="veiculo.descricao" name="descricao" label="Descrição"></v-text-field>
+        <v-text-field v-model="veiculo.preco" name="preco" label="Preço"></v-text-field>
+
+        <v-container class="d-flex justify-center">
+            <v-btn @click="salvar" class="bg-surface-variant">
+                <p v-if="!isEditando">Adicionar</p>
+                <p v-else>Atualizar</p>
+            </v-btn>
+            <v-btn @click="limpar" class="bg-surface-variant">Limpar</v-btn>
+        </v-container>
+    </v-form>
+    <hr />
+
+    <v-table density="comfortable">
+        <TableHeaderList :th_text="theader_text" />
+        <tbody>
+            <tr v-for="veiculo in veiculos" :key="veiculo.name" @click="editar(veiculo)">
+                <td>{{ veiculo.id }}</td>
+                <td>{{ veiculo.cor }}</td>
+                <td>{{ veiculo.modelo }}</td>
+                <td>{{ veiculo.ano }}</td>
+                <td>{{ veiculo.descricao }}</td>
+                <td>{{ veiculo.preco }}</td>
+                <td><button @click="excluir(veiculo.id)" class="excluir">X</button></td>
+            </tr>
+        </tbody>
+    </v-table>
 </template>
 
+<style>
+table {
+    text-align: center !important;
+}
 
-<style scoped></style>
+thead tr th {
+    text-align: center !important;
+    color: #fff;
+    background-color: #a6a29a;
+    text-transform: uppercase;
+}
+
+.excluir {
+    border-radius: 20px;
+    height: 50%;
+    width: 50%;
+    background-color: red;
+}
+</style>
