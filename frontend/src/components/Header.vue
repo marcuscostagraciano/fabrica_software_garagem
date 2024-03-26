2a45d9e768921a7e582b0625ca2618a18aeedf16
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-26 14:36:43 -0300

    responsividade e icones

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,5 +1,27 @@
 <script setup>
+import { onBeforeMount, ref } from 'vue';
 import { routes } from '../router'
+
+const windowWidth = ref(0)
+const isMobile = ref(false)
+const mobileNav = ref(false)
+
+function checkScreenWidth() {
+    windowWidth.value = window.innerWidth
+    if (windowWidth.value < 1130) {
+        isMobile.value = true
+    }
+    else {
+        isMobile.value = false
+        mobileNav.value = false
+    }
+    console.log(windowWidth.value);
+}
+
+onBeforeMount(() => {
+    window.addEventListener("resize", checkScreenWidth)
+    checkScreenWidth()
+})
 </script>
 
 <template>
@@ -7,10 +29,27 @@ import { routes } from '../router'
         <v-col cols="5">
             Garagem app
         </v-col>
+
         <v-col>
-            <v-btn v-for="route in routes" class="px-2 py-0">
+            <v-btn v-show="!isMobile" v-for="route in routes" class="px-2 py-0">
                 <router-link :to="route.path">{{ route.name }}</router-link>
             </v-btn>
+        </v-col>
+
+        <v-col v-show="isMobile" class="text-right" id="mobile-navbar">
+            <v-menu location="bottom">
+                <template v-slot:activator="{ props }">
+                    <v-btn color="primary" dark v-bind="props" icon="mdi-menu">
+                    </v-btn>
+                </template>
+
+                <v-list>
+                    <v-list-item v-for="route in routes">
+                        <v-list-item-title class="text-button text-center"><router-link :to="route.path">{{ route.name
+                                }}</router-link></v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
         </v-col>
     </v-app-bar>
 </template>
@@ -18,5 +57,9 @@ import { routes } from '../router'
 <style scoped>
 .v-col {
     text-align: center;
+}
+
+a {
+    text-decoration: none;
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,17 +4,7 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import { router } from './router';
 import './plugins/axios';
-
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-    components,
-    directives,
-})
+import { vuetify } from './plugins/vuetify';
 
 const app = createApp(App);
 
